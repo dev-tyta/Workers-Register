7689f97a3429529999b0729ed534fb6c04cac1a7
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-02-03 02:30:54 +0100

    Encryption

---
+++ src/data/data_encryption.py
@@ -0,0 +1,23 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+
+class DataEncryption:
+
+    def __init__(self):
+        self.key = get_random_bytes(16)
+        self.cipher = AES.new(self.key, AES.MODE_EAX)
+
+    def encrypt(self, file_name):
+        with open(file_name, "rb") as f:
+            data = f.read()
+        ciphertext, tag = self.cipher.encrypt_and_digest(data)
+
+        with open(file_name + ".enc", "wb") as f:
+            [f.write(x) for x in (self.cipher.nonce, tag, ciphertext)]
+
+    def decrypt(self, file_name):
+        with open(file_name, "rb") as f:
+            nonce, tag, ciphertext = [f.read(x) for x in (16, 16, -1)]
+
+        self.cipher = AES.new(self.key, AES, nonce)
